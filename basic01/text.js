387d7199722c6da27c1bdad8102df240da576be8
SameerkumarDebata <sameerkudebata@gmail.com>
2025-02-07 17:55:56 +0000

    datatypes

---
+++ basic01/text.js
@@ -0,0 +1,20 @@
+"use strict";//treat all jscode as newer version
+// alert(3+3) //we are using nodejs,not browser
+console.log(3+3)
+
+
+//number =>2 to power53
+//bigint
+//string=>""
+//boolean=>True or false
+//null=>standlone value
+//undefined=>
+//symbol=>unique
+
+
+//object
+let age=93
+console.log(typeof age );
+console.log(typeof null);
+
+
